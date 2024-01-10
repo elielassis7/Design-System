adcb7529f6f0301951f0c7b8301f90a1df0c2cb7
Eliel Assis <eliel.assis7@hotmail.com>
2024-01-10 11:10:27 -0300

    Create stories of Tokens

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,4 +1,5 @@
 import { colors } from "@ignite-ui/tokens"
+import { getContrast } from 'polished'
 
 
 export function ColorsGrid() {
@@ -6,7 +7,13 @@ export function ColorsGrid() {
   return Object.entries(colors).map(([key, color]) => {
     return (
       <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
-        <div style={{ display: 'flex', justifyContent: 'space-between', fontFamily: 'monospace', color: '#fff' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF'
+          }}>
           <strong>${key}</strong>
           <span>{color}</span>
         </div>
