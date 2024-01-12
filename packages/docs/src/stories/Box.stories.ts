ee0980da463580687fa9d266743c349970228847
Eliel Assis <eliel.assis7@hotmail.com>
2024-01-12 09:26:54 -0300

    component Box

---
+++ packages/docs/src/stories/Box.stories.ts
@@ -0,0 +1,13 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surface/Box',
+  component: Box,
+  args: {}
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {
+
+}
+
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,36 +1,5 @@
-import { styled } from './styles'
-import { ComponentProps } from 'react'
+export * from './components/Box'
 
 
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite300',
-  borderRadius: '$sm',
-  border: 0,
-  fontWeight: 'bold',
-  color: '$white',
 
-  '&:hover': {
-    backgroundColor: '$ignite500'
-  },
-
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: "$2 $4",
-      },
-      big: {
-        fontSize: 16,
-        padding: "$3 $6",
-      }
-    }
-  },
-
-  defaultVariants: {
-    size: 'small'
-  }
-})
-
-export type ButtonProps = ComponentProps<typeof Button>
 
