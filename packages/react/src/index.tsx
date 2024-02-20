f87f5529a6e4bb960736453f718d820385b15f45
Eliel Assis <eliel.assis7@hotmail.com>
2024-02-20 13:59:33 -0300

    change react/src/index

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,8 +7,8 @@ export * from './components/TextInput/Index'
 export * from './components/TextArea'
 export * from './components/Checkbox/Index'
 export * from './components/MultiStep/Index'
-export * from './components/Tooltip/Index'
-export * from './components/Toast/Index'
+export * from './components/Tooltip'
+export * from './components/Toast'
 
 export * from './styles'
 
